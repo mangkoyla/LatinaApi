131593db57c8e97978cccb2807da0fd07e8c2aa6
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2023-01-24 10:13:45 +0700

    Integrate with docs

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,17 @@
+FROM node:latest as web_builder
+
+WORKDIR /usr/src/web
+
+RUN git clone https://github.com/LalatinaHub/LatinaDocs .
+RUN npm install
+RUN npm run build
+
 FROM golang:latest
 
 WORKDIR /usr/src/app
 
 COPY . .
+COPY --from=web_builder /usr/src/web/docs/.vitepress/dist/ /usr/src/app/public/
 
 RUN go mod download && go mod tidy && go mod verify
 RUN go build -o ./latinaapi ./cmd/latinaapi/main.go
--- api/router/router.go
+++ api/router/router.go
@@ -2,10 +2,10 @@ package router
 
 import (
 	"fmt"
-	"net/http"
 	"os"
 
 	endpoint "github.com/LalatinaHub/LatinaApi/api/get"
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,24 +17,7 @@ var (
 func Start() {
 	Router.SetTrustedProxies(nil)
 
-	Router.StaticFile("/favicon.ico", "resources/favicon.ico")
-
-	Router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"status":  "OK",
-			"message": "Go to /get to get nodes",
-			"query": gin.H{
-				"cc":     "Country Code. ex: SG, ID, JP (select one)",
-				"region": "Regional. ex: Asia, Americas, Europe, Africa (select one)",
-				"vpn":    "Vpn Type, ex: trojan, vmess, vless (select one)",
-				"format": "Target format, ex: clash, surfboard, singbox, raw (select one)",
-				"cdn":    "CDN bugs, separate with (,) for multiple bugs",
-				"sni":    "SNI bugs, separate with (,) for multiple bugs",
-			},
-			"info":    "Parameter is case sensitive",
-			"example": "http://fool.azurewebsites.net/get?vpn=trojan&cdn=hohm.microsoft.com&sni=google.com&cc=SG&format=raw",
-		})
-	})
+	Router.Use(static.Serve("/", static.LocalFile("public/", false)))
 
 	Router.GET("/get", endpoint.GetHandler)
 
