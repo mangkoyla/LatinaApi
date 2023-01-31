21c3ac57fa40c86aea2cc48cf0ee47bf1c0d1cc1
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2023-02-01 04:58:56 +0700

    Decode query value on surfboard config

--- api/helper/helper.go
+++ api/helper/helper.go
@@ -2,6 +2,7 @@ package apiHelper
 
 import (
 	"fmt"
+	"net/url"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,9 +17,9 @@ func GetRequestedURL(c *gin.Context) string {
 	result := schema + c.Request.Host + c.Request.URL.Path
 	for key, value := range c.Request.URL.Query() {
 		if queries == "" {
-			queries = fmt.Sprintf("?%s=%s", key, value[0])
+			queries = fmt.Sprintf("?%s=%s", key, url.QueryEscape(value[0]))
 		} else {
-			queries = fmt.Sprintf("%s&%s=%s", queries, key, value[0])
+			queries = fmt.Sprintf("%s&%s=%s", queries, key, url.QueryEscape(value[0]))
 		}
 	}
 
