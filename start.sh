6d461659455d28c34900e1a4687841a788b3c6e2
mangkoyla <160372172+mangkoyla@users.noreply.github.com>
2024-04-21 23:45:26 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Update latina modules
-go get -v github.com/mangkoyla/LatinaBot@main
-go get -v github.com/mangkoyla/LatinaSub-go@main
+go get -v github.com/mangkoyla/LatinaBot@gh-pages
+go get -v github.com/mangkoyla/LatinaSub-go@gh-pages
 
 # Tidy and verify all modules
 go mod download && go mod tidy && go mod verify
